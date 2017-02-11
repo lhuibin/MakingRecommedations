925b31bfbb6c5218e49303a2a27dc9a2dd99b278
Amo-mac bookair <1018367@qq.com>
2017-02-11 22:02:45 +0800

    01

---
+++ recommendations.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+
+# 一个设计影评着及其对几部影片评分情况对字典
+critics = {
+	'Lisa Rose':{'Lady in the Water':2.5, 'Snakes on a Plane':3.5, 'Just My Luck': 3.0, 'Superman Returns':3.5, 'You, Me and Dupree':2.5, 'The Night Listener':3.0},
+	'Gene Seymour':{'Lady in the water':3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener':3.0,'You, Me and Dupree': 3.5},
+	'Michael Phillips':{'Lady in the Water':2.5, 'Snakes on a Plane':3.0, 'Superman Returns':3.5, 'The Night Listener':4.0}
+	'Claudia Puig':{'Snakes on a Plane':3.5, 'Just My Luck': 3.0, 'Superman Returns':4.0, 'You, Me and Dupree':2.5, 'The Night Listener':4.5}
+	'Jack Matthews':{'Lady in the Water':3.0, 'Snakes on a Plane':4.0, 'Superman Returns':5.0, 'You, Me and Dupree':3.5, 'The Night Listener':3.0}
+	'Toby':{'Snakes on a Plane':4.5, 'Superman Returns':4.0, 'You, Me and Dupree':1.0}
