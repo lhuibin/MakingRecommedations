a28c441ee5931259b1713cffe16c7cf6787d9d83
Amo-mac bookair <1018367@qq.com>
2017-02-12 12:57:49 +0800

    增加皮尔逊相关系数

--- recommendations.py
+++ recommendations.py
@@ -24,3 +24,34 @@ def sim_distance(prefs,person1,person2):
 	sum_of_squares = sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
 
 	return 1/(1+sqrt(sum_of_squares))
+
+# 返回p1 个p2的皮尔逊相关系数
+def sim_pearson(prefs,p1,p2):
+	# 得到双方都曾评价过的物品列表
+	si={}
+	for item in prefs[p1]:
+		if item in prefs[p2]:
+			si[item]=1
+	# 得到列表元素的个数
+	n=len(si)
+	# 如果两者没有共同之处， 则返回1
+	if n==0:
+		return 1
+	# 对所有偏好求和
+	sum1=sum([prefs[p1][it] for it in si])
+	sum2=sum([prefs[p2][it] for it in si])
+
+	#求平方和
+	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
+	# 求乘积之和
+	pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+	# 计算皮尔逊评价值
+	num=pSum-(sum1*sum2/n)
+	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+	if den==0:
+		return 0
+	r=num/den
+	return r
