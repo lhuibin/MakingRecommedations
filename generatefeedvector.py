7047bb3dd7f8881e36282122a47d505cfbcbcea9
mac linux ubnu <1018367@qq.com>
2017-02-15 22:00:06 +0800

    update

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -1,63 +1,63 @@
-#coding:utf-8
 import feedparser
 import re
 
-# 返回一个RSS订阅源的标题和饱含单词技术情况的词典
+# Returns title and dictionary of word counts for an RSS feed
 def getwordcounts(url):
-	# 解析订阅源
-	d=feedparser.parse(url)
-	wc={}
+  # Parse the feed
+  d=feedparser.parse(url)
+  wc={}
 
-	# 循环便利所有的文章条目
-	for e in d.enties:
-		if 'summary' in e:
-			summary=e.summary
-		else:
-			summary=e.description
-		# 提取一个单词列表
-		words=getwords(e.title+' '+summary)
-		for word in words:
-			wc.setdefault(word,0)
-			wc[word]+=1
-		return d.feed.title,wc
+  # Loop over all the entries
+  for e in d.entries:
+    if 'summary' in e: summary=e.summary
+    else: summary=e.description
+
+    # Extract a list of words
+    words=getwords(e.title+' '+summary)
+    for word in words:
+      wc.setdefault(word,0)
+      wc[word]+=1
+  return d.feed.title,wc
 
 def getwords(html):
-	# 去除所有的HTML标记
-	txt=re.compile(r'<[^>]+>').sub(' ',html)
+  # Remove all the HTML tags
+  txt=re.compile(r'<[^>]+>').sub('',html)
 
-	# 利用所有非字母自负拆分出单词
-	words=re.compile(r'[^A-Z^a-z]+').split(txt)
+  # Split words by all non-alpha characters
+  words=re.compile(r'[^A-Z^a-z]+').split(txt)
 
-	# 转化为小写形式
-	return [word.loer() for word in words if word!='']
+  # Convert to lowercase
+  return [word.lower() for word in words if word!='']
+
 
 apcount={}
 wordcounts={}
 feedlist=[line for line in file('feedlist.txt')]
 for feedurl in feedlist:
-	title,wc=getwordcounts(feedurl)
-	wordcounts[title]=wc
-	for word, count in wc.items():
-		apcount.setdefault(word,0)
-		if count>1:
-			apcount[word]+=1
+  try:
+    title,wc=getwordcounts(feedurl)
+    wordcounts[title]=wc
+    for word,count in wc.items():
+      apcount.setdefault(word,0)
+      if count>1:
+        apcount[word]+=1
+  except:
+    print 'Failed to parse feed %s' % feedurl
 
 wordlist=[]
-for w, bc in apcount.items():
-	frac=float(bc)/len(feedlist)
-	if frac>0.1 and frac<0.5:
-		wordlist.append(w)
+for w,bc in apcount.items():
+  frac=float(bc)/len(feedlist)
+  if frac>0.1 and frac<0.5:
+    wordlist.append(w)
 
-out=file('blogdata.txt','w')
+out=file('blogdata1.txt','w')
 out.write('Blog')
-for word in wordlist:
-	out.write('\t%s' % word)
+for word in wordlist: out.write('\t%s' % word)
 out.write('\n')
 for blog,wc in wordcounts.items():
-	out.write(blog)
-	for word in wordlist:
-		if word in wc:
-			out.write('\t%d' % wc[word])
-		else:
-			out.write('\t0')
-	out.write('\n')
+  print blog
+  out.write(blog)
+  for word in wordlist:
+    if word in wc: out.write('\t%d' % wc[word])
+    else: out.write('\t0')
+  out.write('\n')
