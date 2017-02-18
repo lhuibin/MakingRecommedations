53caeba7c30ae855c23427ef3c44e7130b061403
Amo-mac bookair <1018367@qq.com>
2017-02-18 22:19:02 +0800

    updated

--- cluster.py
+++ cluster.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 from math import sqrt
+from PIL import Image,ImageDraw
 
 def readfile(filename):
 	lines=[line for line in file(filename)]
@@ -96,3 +97,62 @@ def printclust(clust,labels=None,n=0):
 		printclust(clust.left,labels=labels,n=n+1)
 	if clust.right!=None:
 		printclust(clust.right,labels=labels,n=n+1)
+
+def getheight(clust):
+	# 这是一个叶节点吗？ 若是，则高度为1
+	if clust.left==None and clust.right==None:
+		return 1
+	# 否则， 高度为每个分支的高度之和
+	return getheight(clust.left)+getheight(clust.right)
+
+def getdepth(clust):
+	# 一个叶节点的距离是0.0
+	if clust.left==None and clust.right==None:
+		return 0
+	# 一个枝节点的距离等于左右两侧分支中距离较大者
+	# 加上改枝节点自身的距离
+	return max(getdepth(clust.left),getdepth(clust.right))+clust.distance
+
+def drawdendrogram(clust,lables,jpeg='clusters.jpg'):
+	# 高度和宽度
+	h=getheight(clust)*20
+	w=1200
+	depth=getdepth(clust)
+
+	# 由于宽度是固定的，因此我们需要对距离值做相应的调整
+	scaling=float(w-150)/depth
+
+	# 新建一个白色背景灯图片
+	img=Image.new('RGB',(w,h),(255,255,255))
+	draw=ImageDraw.Draw(img)
+
+	draw.line((0,h/2,10,h/2),fill=(255,0,0))
+
+	# 画第一个节点
+	drawnode(draw,clust,10,(h/2),scaling,lables)
+	img.save(jpeg,'JPEG')
+
+def drawnode(draw,clust,x,y,scaling,labels):
+	if clust.id<0:
+		h1=getheight(clust.left)*20
+		h2=getheight(clust.right)*20
+		top=y-(h1+h2)/2
+		bottom=y+(h1+h2)/2
+		# 线的长度
+		l1=clust.distance*scaling
+		# 聚类到其子节点的垂直线
+		draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))
+
+		# 连接左侧节点的垂直线
+		draw.line((x,top+h1/2,x+11,bottom-h2/2),fill=(255,0,0))
+
+		# 连接右侧节点的水平线
+		draw.line((x,bottom-h2/2,x+11,bottom-h2/2),fill=(255,0,0))
+
+		# 调用函数绘制左右节点
+		drawnode(draw,clust.left,x+11,top+h1/2,scaling,labels)
+		drawnode(draw,clust.right,x+11,bottom-h2/2,scaling,labels)
+	else:
+		# 如果这是一个叶节点，则绘制节点的标签
+		draw.text((x+5,y-7),labels[clust.id],(0,0,0))
+	pass
